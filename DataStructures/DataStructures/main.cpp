a06076c2c91841bb4f0e84fc9663093e6a891f7a
Sushant Sharma <sushant@sushant-G31M-ES2L.(none)>
2012-06-07 00:25:55 +0530

    Updated for new puzzles.

--- DataStructures/DataStructures/main.cpp
+++ DataStructures/DataStructures/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
 	//simple_list::test();
-	ms_puzzles::bit_stream::test();
+	//ms_puzzles::bit_stream::test();
+	//ms_puzzles::arrays::test_zero_one();
+	ms_puzzles::arrays::test_permutation();
 	return 0;
 }
--- DataStructures/DataStructures/ms_puzzles.h
+++ DataStructures/DataStructures/ms_puzzles.h
@@ -11,11 +11,105 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <unordered_map>
 
 using namespace std;
 
 namespace ms_puzzles {
+	
+	namespace arrays
+	{
+		int randno() { return rand()%200-100; }
+		void test_permutation()
+		{
+			vector<int> v1(30), v2(30);
+			generate(v1.begin(), v1.end(), [](){return rand()%200-100;});
+			copy(v1.begin(), v1.end(), ostream_iterator<int>(cout, ",")); cout << endl;
+			transform(v1.begin(), v1.end(), v1.begin(), bind2nd(modulus<int>(), 100));
+			copy(v1.begin(), v1.end(), ostream_iterator<int>(cout, ",")); cout << endl;
+			v2 = v1;
+			copy(v2.begin(), v2.end(), ostream_iterator<int>(cout, ",")); cout << endl;
+			random_shuffle(v2.begin(), v2.end());
+			copy(v2.begin(), v2.end(), ostream_iterator<int>(cout, ",")); cout << endl;
+			
+			unordered_map<int, int> count;
+			for_each(v1.begin(), v1.end(), [&count](int x) {count[x]++;});
+			for_each(count.begin(), count.end(), [](auto x))
+			for(auto x: count) {
+				cout << x.first << ", "      << x.second << endl;
+			}
+			
+			
+			
+		}
 
+#define SIZE 7
+		
+	  void print_array(int a[][SIZE], int R, int C, int div=10)
+		{
+			for(int i=0; i<R; ++i)
+			{
+				for(int j=0; j<C;++j)
+				{
+					printf("%2d ", a[i][j]%div);
+				}
+				printf("\n");
+			}
+			printf("\n");
+		}
+		
+		void test_zero_one()
+	    {
+			int R(SIZE), C(SIZE), r, c;
+			int a[SIZE][SIZE];
+			for(int i=0; i<R*C; ++i)
+			{
+				r = i/R;
+				c = i%C;
+				a[r][c] = 0;
+			}
+
+			int rn = rand()%SIZE;
+
+			//Radomly set rn elements to 1
+			for(int i=0; i<rn; ++i)
+			{
+			      a[rand()%SIZE][rand()%SIZE] = 1;
+			}
+			a[5][3] = 1;
+			print_array(a, R, C);
+			
+			for(int i=0; i<SIZE; ++i)
+			{
+				for(int j=0; j<SIZE; ++j)
+				{
+				        if (a[i][j]%10==1)
+					{
+					        a[0][j] = 10 + a[0][j]%10;
+					        a[i][0] = 10 + a[i][0]%10;
+					}
+				}
+			}
+			print_array(a, R, C);
+			for(int i=0; i<SIZE; ++i)
+			{
+				for(int j=0; j<SIZE; ++j)
+				{
+				        if ( a[0][j]/10 == 1 || a[i][0]/10 == 1 )
+				        {
+					        int t = a[i][j]/10;
+					        a[i][j] = t*10 + 1;
+				        }
+				}
+			}
+			print_array(a, R, C);
+		}
+	}
+	
 	namespace bit_stream
 	{	
 		struct bit_stream_t
